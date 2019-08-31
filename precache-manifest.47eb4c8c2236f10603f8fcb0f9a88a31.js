b7b017d2fa97f226fa26a32b82005a17afa591a8
aamrits <amritanand888@gmail.com>
2019-08-31 20:18:32 +0530

    Updates

---
+++ precache-manifest.47eb4c8c2236f10603f8fcb0f9a88a31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89b55dca5aaa7e551c9c",
+    "url": "/react-demo-app/static/js/runtime~main.5d1603a4.js"
+  },
+  {
+    "revision": "81094bc72e21f72be308",
+    "url": "/react-demo-app/static/js/main.1fe5e5b3.chunk.js"
+  },
+  {
+    "revision": "5e71b91f0215f257777e",
+    "url": "/react-demo-app/static/js/2.a13078a7.chunk.js"
+  },
+  {
+    "revision": "81094bc72e21f72be308",
+    "url": "/react-demo-app/static/css/main.728859e6.chunk.css"
+  },
+  {
+    "revision": "5e71b91f0215f257777e",
+    "url": "/react-demo-app/static/css/2.43f87b3e.chunk.css"
+  },
+  {
+    "revision": "edb6ba4a95d88e4d6626bb3f5ff6fde7",
+    "url": "/react-demo-app/index.html"
+  }
+];
